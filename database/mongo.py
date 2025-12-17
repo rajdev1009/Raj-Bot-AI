6354cfb3a24d71f4735cf4351179a9092652725a
rajdev1009 <mrdev4772@gmail.com>
2025-12-17 21:05:14 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,6 +1,6 @@
 import motor.motor_asyncio
 from config import Config
-from database.models import User, Log  # <--- Imported Models
+from database.models import User
 from utils.logger import logger
 
 class Database:
@@ -8,22 +8,37 @@ class Database:
         self.client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_URI)
         self.db = self.client.RajBotDB
         
+        # Collections
         self.users = self.db.users
-        self.logs = self.db.logs
+        self.chats = self.db.chats
+        self.voice_logs = self.db.voice_logs
+        self.image_logs = self.db.image_logs
+        self.broadcast_logs = self.db.broadcast_logs
+        self.security_logs = self.db.security_logs
 
     async def add_user(self, user_id, first_name, username):
-        user = await self.users.find_one({"_id": user_id})
-        if not user:
-            # Using the Model here
-            new_user = User(user_id, first_name, username)
-            await self.users.insert_one(new_user.to_dict())
-            logger.info(f"New User Registered: {user_id}")
+        """Adds a new user if they don't exist."""
+        try:
+            user = await self.users.find_one({"_id": user_id})
+            if not user:
+                # Using the User Model class for structure
+                new_user = User(user_id, first_name, username)
+                await self.users.insert_one(new_user.to_dict())
+                logger.info(f"New User Registered: {user_id}")
+                return True
+            return False
+        except Exception as e:
+            logger.error(f"Database Error (add_user): {e}")
 
     async def get_all_users(self):
+        """Fetches all users for broadcast."""
         return [user async for user in self.users.find({})]
 
     async def log_event(self, collection_name, data):
-        # Generic logger
-        await self.db[collection_name].insert_one(data)
+        """Generic logger for events."""
+        try:
+            await self.db[collection_name].insert_one(data)
+        except Exception as e:
+            logger.error(f"Database Log Error: {e}")
 
 db = Database()
