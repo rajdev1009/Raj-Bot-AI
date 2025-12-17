28c44925f1dde8ce14e2d0ba9d26c349f95ae6b8
rajdev1009 <mrdev4772@gmail.com>
2025-12-17 20:37:52 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler("bot_activity.log")
+    ]
+)
+
+logger = logging.getLogger("RajBot")
