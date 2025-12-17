64653b22767bfce820764bc9ff936448d883df68
rajdev1009 <mrdev4772@gmail.com>
2025-12-17 23:24:11 +0530

    Update security.py

--- core/security.py
+++ core/security.py
@@ -1,12 +1,18 @@
 from database.mongo import db
+import random
+import time
 
 user_sessions = {}
 
 class Security:
     PASSWORD = "Rajdev9101"
+    SESSION_DURATION = 3600  # 1 hour
 
-    # CONFIG
-    MY_IMAGE = "https://i.ibb.co/qFyMDRk8/IMG-20251130-WA0108-2.jpg"
+    IMAGES = [
+        "https://i.ibb.co/qFyMDRk8/IMG-20251130-WA0108-2.jpg",
+        "IMAGE_LINK_2",
+        "IMAGE_LINK_3"
+    ]
 
     CHANNEL_NAME = "Raj HD Movies"
     CHANNEL_LINK = "https://t.me/+u4cmm3JmIrFlNzZl"
@@ -18,7 +24,7 @@ class Security:
     FACEBOOK_LINK = "https://www.facebook.com/share/1C794RUagn/"
 
     MY_INFO = (
-        f"📞 **Phone:** +91 9395744401"
+        f"📞 **Phone:** +91 9395744401\n"
         f"📸 **Instagram:** [{INSTAGRAM_NAME}]({INSTAGRAM_LINK})\n"
         f"📘 **Facebook:** [{FACEBOOK_NAME}]({FACEBOOK_LINK})\n"
         f"📍 **Location:** Lumding, Assam"
@@ -27,7 +33,10 @@ class Security:
     @staticmethod
     async def check_password(user_id, text):
         if text == Security.PASSWORD:
-            user_sessions.pop(user_id, None)
+            user_sessions[user_id] = {
+                "status": "authenticated",
+                "time": time.time()
+            }
 
             caption = (
                 "✅ **Access Granted! Welcome Boss.**\n\n"
@@ -35,7 +44,7 @@ class Security:
                 f"👤 **Owner Details:**\n{Security.MY_INFO}"
             )
 
-            return True, caption, Security.MY_IMAGE
+            return True, caption, random.choice(Security.IMAGES)
 
         else:
             await db.log_event(
@@ -47,11 +56,23 @@ class Security:
 
     @staticmethod
     def is_waiting(user_id):
-        return user_sessions.get(user_id) == "awaiting_password"
+        session = user_sessions.get(user_id)
+
+        if not session:
+            return False
+
+        if session.get("status") == "authenticated":
+            if time.time() - session["time"] < Security.SESSION_DURATION:
+                return False
+            else:
+                user_sessions.pop(user_id, None)
+                return True
+
+        return session.get("status") == "awaiting_password"
 
     @staticmethod
     def initiate_auth(user_id):
-        user_sessions[user_id] = "awaiting_password"
+        user_sessions[user_id] = {"status": "awaiting_password"}
         return (
             "🔒 **Security Check**\n\n"
             "Hidden content dekhne ke liye Password enter karein:"
