1e2439f57e8024c8beb25fd2a888875f0ae1fa64
rajdev1009 <mrdev4772@gmail.com>
2025-12-17 20:58:54 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,6 +1,6 @@
 import motor.motor_asyncio
 from config import Config
-from datetime import datetime
+from database.models import User, Log  # <--- Imported Models
 from utils.logger import logger
 
 class Database:
@@ -8,31 +8,22 @@ class Database:
         self.client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_URI)
         self.db = self.client.RajBotDB
         
-        # Collections
         self.users = self.db.users
-        self.chats = self.db.chats
-        self.voice_logs = self.db.voice_logs
-        self.image_logs = self.db.image_logs
-        self.broadcast_logs = self.db.broadcast_logs
-        self.security_logs = self.db.security_logs
+        self.logs = self.db.logs
 
     async def add_user(self, user_id, first_name, username):
         user = await self.users.find_one({"_id": user_id})
         if not user:
-            await self.users.insert_one({
-                "_id": user_id,
-                "first_name": first_name,
-                "username": username,
-                "joined_at": datetime.now()
-            })
+            # Using the Model here
+            new_user = User(user_id, first_name, username)
+            await self.users.insert_one(new_user.to_dict())
             logger.info(f"New User Registered: {user_id}")
 
     async def get_all_users(self):
         return [user async for user in self.users.find({})]
 
     async def log_event(self, collection_name, data):
-        collection = getattr(self, collection_name)
-        data['timestamp'] = datetime.now()
-        await collection.insert_one(data)
+        # Generic logger
+        await self.db[collection_name].insert_one(data)
 
 db = Database()
